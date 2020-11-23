f68acabda0d8980991461ef8b3b44bfef2cc2f11
Daria <dsnaydina@gmail.com>
2020-11-23 23:29:40 +0100

    different types of ID were added, as i never thought about other people dealing teith the post. Actions are added, as i thought they are supposed to be in a separete class.

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,7 +1,10 @@
 package ru.netology.domain;
 
 public class Post {
-    private int id;
+    private int postId;
+    private int wallId;
+    private int userId;
+    private int postRepliersId;
     private String userName;
     private String avatarUrl;
     private String name;
@@ -12,6 +15,96 @@ public class Post {
     private int rePost;
     private String imageUrl;
     private int viewsAmount;
+    private int attachment;
+    private String geo;
+    private boolean canEdit;
+    private boolean canDelete;
+    private boolean canPin;
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getWallId() {
+        return wallId;
+    }
+
+    public void setWallId(int wallId) {
+        this.wallId = wallId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getPostRepliersId() {
+        return postRepliersId;
+    }
+
+    public void setPostRepliersId(int postRepliersId) {
+        this.postRepliersId = postRepliersId;
+    }
+
+    public int getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(int attachment) {
+        this.attachment = attachment;
+    }
+
+    public String getGeo() {
+        return geo;
+    }
+
+    public void setGeo(String geo) {
+        this.geo = geo;
+    }
+
+    public boolean isCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(boolean canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    public boolean isCanDelete() {
+        return canDelete;
+    }
+
+    public void setCanDelete(boolean canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public boolean isCanPin() {
+        return canPin;
+    }
+
+    public void setCanPin(boolean canPin) {
+        this.canPin = canPin;
+    }
+
+    public boolean isFavoured() {
+        return isFavoured;
+    }
+
+    public void setFavoured(boolean favoured) {
+        isFavoured = favoured;
+    }
+
+    private boolean isFavoured;
+
+
+
 
     public String getUserName() {
         return userName;
