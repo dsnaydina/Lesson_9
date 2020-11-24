3d9d433cc6f452f19f0253ad361ed9aaafbc9070
Daria <dsnaydina@gmail.com>
2020-11-24 21:45:52 +0100

    PostManager is added

---
+++ src/main/java/ru/netology/domain/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] deletePostFromFeed() {
+        // TODO: add logic
+        return null;
+    }
+
+    public Post[] searchPost() {
+        // TODO: add logic
+        return null;
+    }
+}
